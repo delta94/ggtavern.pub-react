20ccf1a8d0048a5786509feb4fae08f59dc47cd8
Zachary Bryant <Zachary@admiralfeb.net>
2020-08-22 17:17:59 -0500

    test: update app test

--- src/app/App.spec.tsx
+++ src/app/App.spec.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
 
-test('renders learn react link', () => {
-  expect(true).toBe(true);
+let container: HTMLDivElement = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+test('renders', () => {
+  act(() => {
+    render(<App />, container);
+  });
 });
