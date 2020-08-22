add57fe40cd4ec27c980ad4272b10f3c3856259e
Zachary Bryant <Zachary@admiralfeb.net>
2020-08-21 23:40:42 -0500

    refactor: app.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import './App.scss';
-import UnderConstruction from 'app/shared/UnderConstruction';
+import UnderConstruction from 'app/shared/components/UnderConstruction';
+import NavbarComponent from 'app/core/NavbarComponent';
+
+import { ThemeProvider, CssBaseline } from '@material-ui/core';
+import { darkTheme } from './theme';
+import { Router, Route, Redirect } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 
 function App() {
+  const history = createBrowserHistory({ basename: process.env.PUBLIC_URL });
   return (
-    <div className='App'>
-      <UnderConstruction title='Grinning Goblin Gaming Tavern'></UnderConstruction>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Router history={history}>
+        <NavbarComponent />
+        <Route path='/home'>
+          <UnderConstruction title='Grinning Goblin Gaming Tavern'></UnderConstruction>
+        </Route>
+        <Route path='/about'>
+          <UnderConstruction title='About Us'></UnderConstruction>
+        </Route>
+        <Redirect exact path='/' to='/home' />
+      </Router>
+    </ThemeProvider>
   );
 }
 
---
+++ src/app/theme.ts
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core';
+
+export const darkTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#9d6c33',
+    },
+    secondary: {
+      main: '#7baf5b',
+    },
+    type: 'dark',
+  },
+  overrides: {},
+});
