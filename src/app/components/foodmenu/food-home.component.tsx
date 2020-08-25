243f91d2cb32556bcc7d7292bf42291f7b43dd69
Zachary Bryant <Zachary@admiralfeb.net>
2020-08-25 18:23:14 -0500

    refactor: rm unused variable in food-home

--- src/app/components/foodmenu/food-home.component.tsx
+++ src/app/components/foodmenu/food-home.component.tsx
@@ -22,18 +22,6 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const rules = [
-  'Playing games, unless otherwise stated, is free.',
-  'VR costs $10/hour. $1/hr on Sundays',
-  'You can bring your own games or systems.',
-  `We ask you not to bring Xbox One or Playstation 4 games as they require installation on the system.`,
-  'We have 3 Wii U systems, but only one of each of the others.',
-  'Please do NOT move systems between the TVs.',
-  'The bartender will hold onto your ID during use of a Tavern game.',
-  'The lists are not extensive as they are subject to frequent change.',
-  'Feel free to donate games to the Tavern!',
-];
-
 export const FoodHome = () => {
   const classes = useStyles();
   return (
